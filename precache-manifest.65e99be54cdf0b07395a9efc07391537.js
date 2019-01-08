ff496d3767bd99bcf544e31ccd702d4964a0e28b
Aleksei Verkholantcev <lyosha@fb.com>
2019-01-08 10:07:57 +0000

    Updates

---
+++ precache-manifest.65e99be54cdf0b07395a9efc07391537.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e96a486204ff57e2541f",
+    "url": "/minesweeper/static/js/runtime~main.e96a4862.js"
+  },
+  {
+    "revision": "af487993cc331bb7f299",
+    "url": "/minesweeper/static/js/main.af487993.chunk.js"
+  },
+  {
+    "revision": "9b0c0ec148d0349e1e3d",
+    "url": "/minesweeper/static/js/1.9b0c0ec1.chunk.js"
+  },
+  {
+    "revision": "af487993cc331bb7f299",
+    "url": "/minesweeper/static/css/main.8ac0394f.chunk.css"
+  },
+  {
+    "revision": "82c38fb368653459030976d5b1cfa53a",
+    "url": "/minesweeper/index.html"
+  }
+];
